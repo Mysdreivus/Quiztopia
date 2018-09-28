f3438c9d9ae1d038491c9a708b932d126102f663
Henchhing Limbu <henchhing@henchhing-macbookpro.roam.corp.google.com>
2018-09-28 14:27:18 -0700

    Updated certain files

--- login.js
+++ login.js
@@ -7,4 +7,26 @@ function login() {
     var password = document.getElementById("pwd_field").value;
     window.alert("email " + email);
     window.alert("password " + password);
+
+    if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+    }
+    if (password.length < 1) {
+        alert('Please enter a password.');
+        return;
+    }
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode == 'auth/wrong-password') {
+            alert('Wrong password');
+        } else {
+            alert(errorMessage)
+        }
+        console.log(error);
+    });
 }
