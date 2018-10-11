db4026dc2b25f47da2aa14c78370e798883b939a
Henchhing Limbu <henchhing@henchhing-macbookpro.roam.corp.google.com>
2018-10-11 09:46:59 -0700

    Implemented password reset; added background image to the webpage

--- signin.js
+++ signin.js
@@ -2,12 +2,9 @@ function  signUp() {
     location.href="signup.html";
 }
 
-function login() {
+function signin() {
     var email = document.getElementById("email_field").value;
     var password = document.getElementById("pwd_field").value;
-    window.alert("email " + email);
-    window.alert("password " + password);
-
     if (email.length < 4) {
         alert('Please enter an email address.');
         return;
@@ -17,16 +14,20 @@ function login() {
         return;
     }
 
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-
         if (errorCode == 'auth/wrong-password') {
             alert('Wrong password');
         } else {
-            alert(errorMessage)
+            alert(errorMessage);
         }
-        console.log(error);
-    });
+        return;
+        });
+    var user = firebase.auth().currentUser;
+    if (user) {
+        window.location.href = "home.html";
+    }
 }
+
--- signup.js
+++ signup.js
@@ -1,17 +1,10 @@
 function signUpCreate() {
     var email = document.getElementById("email_field").value;
     var password = document.getElementById("pwd_field").value;
-    var user_name = document.getElementById("user_name");
-
-    console.log("Email: " + email);
-    console.log("Password: " + password);
-    console.log("User Name: " + user_name);
-
-    if (email.length < 4) {
+    if (email.length < 11) {
         alert('Please enter an email address.');
         return;
     }
-    alert("Checking for validity of the password!");
     if (!isPwdValid(password)) {
         alert("Error:\n" +
             "A password\n" +
@@ -20,23 +13,22 @@ function signUpCreate() {
             "* contains at least one upper case character\n");
         return;
     }
-
-    // Sign in with email and pass.
-    // [START createwithemail]
-    alert("Creating a new user");
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode == 'auth/weak-password') {
-            alert("The password is too weak.");
-        }
-        else {
-            alert(error.message);
-        }
-        console.log(error);
-    });
-    alert("Created a new user!");
+    var user;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/weak-password') {
+                alert("The password is too weak.");
+            }
+            else {
+                alert(error.message);
+            }
+        })
+        .then(function () {
+            window.location.href="home.html";
+        })
+    ;
 }
 
 // matches a string of six or more characters
