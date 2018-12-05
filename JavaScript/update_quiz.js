d6010f06e3b684e6e36a18d0d524d8ad05efcd7d
Henchhing Limbu <henchhing@henchhing-macbookpro2.roam.corp.google.com>
2018-12-04 21:29:40 -0800

    fixes the backend in create_quiz and update_quiz

--- JavaScript/update_quiz.js
+++ JavaScript/update_quiz.js
@@ -95,25 +95,16 @@ function submitQuiz() { // TODO -> Determine how it will be implemented...
 }
 
 function submitQuizHelper(userId) {
-    let questionIds = ["question_1" /*, "question_2", "question_3", "question_4", "question_5", "question_6"
-                        ,"question_7", "question_8", "question_9", "question_10" */];
+    let questionIds = ["question_1" , "question_2", "question_3", "question_4", "question_5"];
 
-    let correctAnswerIds = ["correct_answer_1" /*, "correct_answer_2", "correct_answer_3", "correct_answer_4"
-                            ,"correct_answer_5", "correct_answer_6", "correct_answer_7", "correct_answer_8"
-                            ,"correct_answer_9", "correct_answer_10" */];
+    let correctAnswerIds = ["correct_answer_1" , "correct_answer_2", "correct_answer_3", "correct_answer_4"
+                            ,"correct_answer_5"];
 
     let wrongAnswersIds = [["wrong_answer1_1", "wrong_answer1_2", "wrong_answer1_3"]
-        /*
                             ,["wrong_answer2_1", "wrong_answer2_2", "wrong_answer2_3"]
                             ,["wrong_answer3_1", "wrong_answer3_2", "wrong_answer3_3"]
                             ,["wrong_answer4_1", "wrong_answer4_2", "wrong_answer4_3"]
-                            ,["wrong_answer5_1", "wrong_answer5_2", "wrong_answer5_3"]
-                            ,["wrong_answer6_1", "wrong_answer6_2", "wrong_answer6_3"]
-                            ,["wrong_answer7_1", "wrong_answer7_2", "wrong_answer7_3"]
-                            ,["wrong_answer8_1", "wrong_answer8_2", "wrong_answer8_3"]
-                            ,["wrong_answer9_1", "wrong_answer9_2", "wrong_answer9_3"]
-                            ,["wrong_answer10_1", "wrong_answer10_2", "wrong_answer10_3"]
-                            */];
+                            ,["wrong_answer5_1", "wrong_answer5_2", "wrong_answer5_3"]];
 
     let quiz_name = document.getElementById("quiz_name").value;
     let category = document.getElementById("category").value;
@@ -130,7 +121,7 @@ function submitQuizHelper(userId) {
             wrong_answers3: document.getElementById(wrongAnswersIds[i][2]).value
         };
     }
-    alert("Author name is: " + author);
+
     let data = {
         active: true,
         name: quiz_name,
